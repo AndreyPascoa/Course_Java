60a54199f38ee31a2201e5f0014e8e317d72bdb0
AndreyPascoa <andrey.pascoa@gmail.com>
2023-10-18 15:24:50 -0300

    HotelResevation

---
+++ src/Resevation.java
@@ -0,0 +1,50 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Resevation {
+    
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+    
+    public Resevation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration(){
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MICROSECONDS);
+    }
+
+    public void updateDates(Date checkIn, Date checkOut){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "Room" + roomNumber + "\ncheckIn: " + sdf.format(checkIn) + "\ncheckOut: " + sdf.format(checkOut) + "\n" + duration() + "nights";
+    }
+
+}
